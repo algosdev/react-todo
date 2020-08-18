0536086806647bddf1f5522e22f05db9adc122ca
prince-algo <samandarofficial149@gmail.com>
2020-08-18 18:52:36 +0500

    Functional Components

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Filter extends Component {
 
-    changeHandler = (e) => {
-        this.props.searchFunc(e.target.value)
+const Filter = ({ listChange, searchFunc }) => {
+
+    const changeHandler = (e) => {
+        searchFunc(e.target.value)
     }
-    render() {
-        return (
-            <div className='filter-area'>
-                <input type='text' onChange={this.changeHandler} placeholder='Search for the task' />
-                <div className='btn-group'>
-                    <button className='yellow' onClick={() => {
-                        this.props.listChange('all')
-                    }}>All</button>
-                    <button className='blue' onClick={() => {
-                        this.props.listChange('active');
-                    }}>Active</button>
-                    <button className='green' onClick={() => {
-                        this.props.listChange('done')
-                        this.setState({
-                            activeIndex: 2
-                        })
-                    }}>Done</button>
-                </div>
-            </div >
-        )
-    }
+
+    return (
+        <div className='filter-area'>
+            <input type='text' onChange={changeHandler} placeholder='Search for the task' />
+            <div className='btn-group'>
+                <button className='yellow' onClick={() => {
+                    listChange('all')
+                }}>All</button>
+                <button className='blue' onClick={() => {
+                    listChange('active');
+                }}>Active</button>
+                <button className='green' onClick={() => {
+                    listChange('done')
+                    this.setState({
+                        activeIndex: 2
+                    })
+                }}>Done</button>
+            </div>
+        </div >
+    )
 }
 
 export default Filter
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
 
-class Header extends Component {
-    render() {
-        return (
-            <div className='header'>
-                <h1>Algos Todo</h1>
-                <p>{this.props.active} more to do, {this.props.done} done</p>
-            </div>
-        )
-    }
+const Header = ({ active, done }) => {
+    return (
+        <div className='header'>
+            <h1>Algos Todo</h1>
+            <p>{active} more to do, {done} done</p>
+        </div>
+    )
 }
 
 export default Header
